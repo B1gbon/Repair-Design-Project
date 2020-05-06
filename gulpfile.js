3702eb0450e228446b48039bbdfeaa6bab79d469
Alexandr <aldr4@bk.ru>
2020-05-06 18:29:57 +0300

    feat: add gulp-sass in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,24 @@
-function defaultTask(cb) {
-  // place code for your default task here
-  cb();
-}
+const {src, dest, watch} = require('gulp');
+const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
 
-exports.default = defaultTask
+function bs() {
+  serveSass();
+  browserSync.init({
+      server: {
+          baseDir: "./src/"
+      }
+  });
+  watch("./*.html").on('change', browserSync.reload);
+  watch("./sass/**/*.sass", serveSass);
+  watch("./js/*.js").on('change', browserSync.reload);
+};
+
+function serveSass() {
+  return src("./sass/*.sass")
+      .pipe(sass())
+      .pipe(dest("./css"))
+      .pipe(browserSync.stream());
+};
+
+exports.serve = bs;
