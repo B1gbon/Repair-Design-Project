bb40627b259a3fd01c06689907eca870f0ed2d3a
Alexandr <aldr4@bk.ru>
2020-05-25 15:36:45 +0300

    feat: section project

--- src/js/main.js
+++ src/js/main.js
@@ -68,7 +68,25 @@ $(document).ready(function () {
 $('.upbutton').click(function() {
     $('html, body').stop().animate({scrollTop : 0}, 300);
 });
+var mySwiper = new Swiper ('.swiper-container', {
+  
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+})
+  var next = $('.swiper-button-next');
+  var prev = $('.swiper-button-prev');
+  var bullets = $('.swiper-pagination');
 
+
+  next.css('left', prev.width() + 40 + bullets.width() + 40 )
+  bullets.css('left', prev.width() + 40 )
 
 
 });
